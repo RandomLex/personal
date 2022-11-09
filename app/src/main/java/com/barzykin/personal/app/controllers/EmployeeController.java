d0066a60e06769de7cf774de5fd26c07b687d02a
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2022-11-10 00:57:05 +0400

    32-spring-mvc-jsp-xml

--- app/src/main/java/com/barzykin/personal/app/controllers/EmployeeController.java
+++ app/src/main/java/com/barzykin/personal/app/controllers/EmployeeController.java
@@ -1,32 +1,26 @@
 package com.barzykin.personal.app.controllers;
 
 import com.barzykin.personal.app.repositories.EmployeeRepository;
-import com.barzykin.personal.app.repositories.RepositoryFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+@Controller
+public class EmployeeController  {
+    private final EmployeeRepository employeeRepository;
 
-@WebServlet("/employees")
-public class EmployeeController extends AbstractController {
-    private EmployeeRepository employeeRepository;
-
-    @Override
-    public void init(ServletConfig config) throws ServletException  {
-        super.init(config);
-        config.getServletContext().getAttribute("ctx");
-        employeeRepository = ctx.getBean("employeeRepository", EmployeeRepository.class);
+    @Autowired
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
-    @Override
-    public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("employees", employeeRepository.findAll());
-        req.getRequestDispatcher("employees.jsp").forward(req, resp);
+    @RequestMapping(path = "employees", method = RequestMethod.GET)
+    public ModelAndView getAllEmployees() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("employees", employeeRepository.findAll());
+        modelAndView.setViewName("employees");
+        return modelAndView;
     }
 }

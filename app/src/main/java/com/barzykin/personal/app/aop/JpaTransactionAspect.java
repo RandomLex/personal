2e66436cf1337d2ccd7146653881e78819b47bf9
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2022-11-25 00:39:09 +0400

    34-spring-aop-transaction-with-self-created-aspect

---
+++ app/src/main/java/com/barzykin/personal/app/aop/JpaTransactionAspect.java
@@ -0,0 +1,55 @@
+package com.barzykin.personal.app.aop;
+
+import com.barzykin.personal.app.repositories.helpers.EntityManagerHelper;
+import com.barzykin.personal.model.Employee;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class JpaTransactionAspect {
+    private final EntityManagerHelper helper;
+
+    @Around("@annotation(JpaTransaction)") //Advice type around
+    public Object transaction(ProceedingJoinPoint jp) {
+        EntityManager em = null;
+        Object result = null;
+        try {
+            em = helper.getEntityManager();
+            em.getTransaction().begin();
+
+            result = jp.proceed();
+
+            em.getTransaction().commit();
+        } catch (Throwable e) {
+            safeRollback(em);
+        } finally {
+            safeClose(em);
+        }
+        return result;
+    }
+
+    private static void safeRollback(EntityManager em) {
+        EntityTransaction tx;
+        if (em != null && (tx = em.getTransaction()) != null) {
+            tx.rollback();
+        }
+    }
+
+    private static void safeClose(EntityManager em) {
+        if (em != null) {
+            em.close();
+        }
+    }
+
+}
--- app/src/main/java/com/barzykin/personal/app/repositories/EmployeeRepositoryJpa.java
+++ app/src/main/java/com/barzykin/personal/app/repositories/EmployeeRepositoryJpa.java
@@ -1,20 +1,15 @@
 package com.barzykin.personal.app.repositories;
 
-import com.barzykin.demo.complexid.Emp;
+import com.barzykin.personal.app.aop.JpaTransaction;
 import com.barzykin.personal.app.repositories.helpers.EntityManagerHelper;
 import com.barzykin.personal.model.Employee;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
-import javax.persistence.TypedQuery;
-import javax.transaction.Transaction;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Predicate;
 
 @Component
 public class EmployeeRepositoryJpa implements EmployeeRepository {
@@ -26,56 +21,18 @@ public class EmployeeRepositoryJpa implements EmployeeRepository {
     }
 
     @Override
+    @JpaTransaction
     public List<Employee> findAll() {
-        EntityManager em = null;
-        List<Employee> employees = new ArrayList<>();
-        try {
-            em = helper.getEntityManager();
-            em.getTransaction().begin();
-
-            TypedQuery<Employee> query = em.createQuery("from Employee", Employee.class);
-            employees = query.getResultList();
-
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            safeRollback(em);
-        } finally {
-            safeClose(em);
-        }
-        return employees;
+        return helper
+                .getEntityManager()
+                .createQuery("from Employee", Employee.class)
+                .getResultList();
     }
-
-    private static void safeRollback(EntityManager em) {
-        EntityTransaction tx;
-        if (em != null && (tx = em.getTransaction()) != null) {
-            tx.rollback();
-        }
-    }
-
-    private static void safeClose(EntityManager em) {
-        if (em != null) {
-            em.close();
-        }
-    }
-
 
     @Override
+    @JpaTransaction
     public Optional<Employee> find(long id) {
-        EntityManager em = null;
-        Optional<Employee> employeeOpt = Optional.empty();
-        try {
-            em = helper.getEntityManager();
-            em.getTransaction().begin();
-
-            employeeOpt = Optional.ofNullable(em.find(Employee.class, id));
-
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            safeRollback(em);
-        } finally {
-            safeClose(em);
-        }
-        return employeeOpt;
+        return Optional.ofNullable(helper.getEntityManager().find(Employee.class, id));
     }
 
     @Override
@@ -128,5 +85,18 @@ public class EmployeeRepositoryJpa implements EmployeeRepository {
             safeClose(em);
         }
         return employeeOpt;
+    }
+
+    private static void safeRollback(EntityManager em) {
+        EntityTransaction tx;
+        if (em != null && (tx = em.getTransaction()) != null) {
+            tx.rollback();
+        }
+    }
+
+    private static void safeClose(EntityManager em) {
+        if (em != null) {
+            em.close();
+        }
     }
 }
